92a0a224d484da667f720ae01376ae7fb26ae03e
TomeHane <dover1274@gmail.com>
2025-08-05 01:41:26 +0900

    ホームページ（の見せかけ）を実装

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// ホーム画面用JavaScriptファイルを読み込み
+import "custom/home"
